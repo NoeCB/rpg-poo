47205ce7778c342cb6a05754269e1abb8292edff
Noelia Cantador <noe2900@gmail.com>
2026-04-14 09:29:05 +0200

    cambios

--- src/com/dbd/arma/Cuchillo.java
+++ src/com/dbd/arma/Cuchillo.java
@@ -1,14 +1,45 @@
 package com.dbd.arma;
 
+/**
+ * Representa un arma de tipo cuerpo a cuerpo.
+ * <p>
+ * Esta clase es una implementación concreta de {@link Arma}. 
+ * Se caracteriza por ser un arma ágil y rápida, ideal para 
+ * combates a corta distancia, sacrificando poder de daño bruto 
+ * a cambio de mantener una buena probabilidad de acierto.
+ * </p>
+ * * @author Noelia Cantador y Luis Lázaro.
+ * @version 1.0
+ * @see Arma
+ */
 public class Cuchillo extends Arma {
-  public Cuchillo() {
+    
+    /**
+     * Crea una nueva instancia de Cuchillo con valores predeterminados.
+     * <p>
+     * Las estadísticas base para esta arma son:
+     * <ul>
+     * <li><b>Nombre:</b> Cuchillo de Caza</li>
+     * <li><b>Daño:</b> 10</li>
+     * <li><b>Precisión:</b> 80%</li>
+     * </ul>
+     * </p>
+     */
+    public Cuchillo() {
+        // Daño bajo (10), precisión alta (80)
         super("Cuchillo de Caza", 10, 80);
     }
 
+    /**
+     * Ejecuta la acción de ataque con el cuchillo.
+     * <p>
+     * Implementa la lógica específica de daño y cálculo de 
+     * acierto definida por las estadísticas del arma.
+     * </p>
+     */
     @Override
     public void usar() {
-      
+        // Lógica de uso del cuchillo
     }
 
 }
-
